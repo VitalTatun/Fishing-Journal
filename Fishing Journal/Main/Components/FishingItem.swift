f5fb94956d018aff355d39c217c08f8add334043
tatunvital <99530985+tatunvital@users.noreply.github.com>
2023-11-12 17:06:34 +0300

    Create Main View

---
+++ Fishing Journal/Main/Components/FishingItem.swift
@@ -0,0 +1,18 @@
+//
+//  FishingItem.swift
+//  Fishing Journal
+//
+//  Created by Виталий Татун on 12.11.23.
+//
+
+import SwiftUI
+
+struct FishingItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FishingItem()
+}
