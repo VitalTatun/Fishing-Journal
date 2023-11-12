049ee53f191e037bf9cbeaeabe85e26cda217bb4
tatunvital <99530985+tatunvital@users.noreply.github.com>
2023-11-12 12:48:18 +0300

    Initial Commit

---
+++ Fishing Journal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fishing Journal
+//
+//  Created by Виталий Татун on 12.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fishing Journal/Fishing_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fishing_JournalApp.swift
+//  Fishing Journal
+//
+//  Created by Виталий Татун on 12.11.23.
+//
+
+import SwiftUI
+
+@main
+struct Fishing_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
