3f879766f4f76b504f83d9ae4eba62a2ff7f479a
tatunvital <99530985+tatunvital@users.noreply.github.com>
2024-06-26 20:51:24 +0300

    UI - Fishing Type icon

--- Fishing Journal/DetailFishingView/Subviews/Header.swift
+++ Fishing Journal/DetailFishingView/Subviews/Header.swift
@@ -21,29 +21,44 @@ struct Header: View {
                     .clipShape(Circle())
                     
                 VStack(alignment: .leading, spacing: 0) {
-                    Text(fishing.name)
-                        .font(.system(.headline, design: .rounded, weight: .semibold))
-                        .foregroundColor(.black)
+                    HStack(spacing: 5) {
+                        Text(fishing.type.name)
+                            .font(.system(.footnote, weight: .medium))
+                            .foregroundStyle(fishing.type.accentColor)
+                            .padding(.horizontal, 10)
+                            .padding(.vertical, 2)
+                            .background(fishing.type.backgroundColor)
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 5)
+                                    .inset(by: 0.5)
+                                    .stroke(fishing.type.accentColor)
+                                    .opacity(0.2)
+                            }
+                        Text(fishing.name)
+                            .font(.system(.headline, design: .rounded, weight: .semibold))
+                            .foregroundColor(.black)
                         .lineLimit(1)
+                    }
                     Text(fishing.user.name)
                         .font(.footnote)
                         .lineLimit(1)
                 }
                 Spacer()
-                Text(fishing.type.name)
-                    .font(.footnote)
-                    .lineLimit(1)
-                    .fontWeight(.medium)
-                    .foregroundStyle(fishing.type.accentColor)
-                    .padding(.horizontal, 14)
-                    .padding(.vertical, 6)
-                    .background(fishing.type.backgroundColor)
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 20)
-                            .inset(by: 0.5)
-                            .stroke(fishing.type.accentColor)
-                    }
+//                Text(fishing.type.name)
+//                    .font(.footnote)
+//                    .lineLimit(1)
+//                    .fontWeight(.medium)
+//                    .foregroundStyle(fishing.type.accentColor)
+//                    .padding(.horizontal, 14)
+//                    .padding(.vertical, 6)
+//                    .background(fishing.type.backgroundColor)
+//                    .clipShape(RoundedRectangle(cornerRadius: 20))
+//                    .overlay {
+//                        RoundedRectangle(cornerRadius: 20)
+//                            .inset(by: 0.5)
+//                            .stroke(fishing.type.accentColor)
+//                    }
             }
             Divider()
         }
